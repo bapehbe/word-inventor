a325070b5dd138abfd4ce7038dc0a97d17b3cea3
Alexey Pushkin <alexey.pushkin@gmail.com>
2012-07-29 12:23:45 +0400

    added german

--- src/word_inventor/config.clj
+++ src/word_inventor/config.clj
@@ -3,4 +3,5 @@
 (def language-sources
   {:ru {:title "Русский" :source "resources/private/russian.txt.gz"}
    :it {:title "L'Italiano" :source "resources/private/italian.txt.gz"}
-   :en {:title "English" :source "resources/private/english.txt.gz"}})
+   :en {:title "English" :source "resources/private/english.txt.gz"}
+   :de {:title "Deutsche" :source "resources/private/german.txt.gz"}})
