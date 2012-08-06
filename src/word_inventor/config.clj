9b7748e30abaebdd39ec18f496f856ebb0b94606
Alexey Pushkin <alexey.pushkin@gmail.com>
2012-08-07 03:40:24 +0400

    added French

--- src/word_inventor/config.clj
+++ src/word_inventor/config.clj
@@ -4,5 +4,6 @@
   {"ru" {:title "Русский" :source "resources/private/russian.txt.gz"}
    "it" {:title "L'Italiano" :source "resources/private/italian.txt.gz"}
    "en" {:title "English" :source "resources/private/english.txt.gz"}
-   "de" {:title "Deutsche" :source "resources/private/german.txt.gz"}})
+   "de" {:title "Deutsche" :source "resources/private/german.txt.gz"}
+   "fr" {:title "Le Français" :source "resources/private/french.txt.gz"}})
 
