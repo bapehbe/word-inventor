e7c7e344ddbfbdccc7c15fcc8bbeee46fef2087a
Alexey Pushkin <alexey.pushkin@gmail.com>
2012-08-12 23:56:14 +0400

    added instructions. changed css

--- src/word_inventor/views/welcome.clj
+++ src/word_inventor/views/welcome.clj
@@ -1,7 +1,14 @@
 (ns word-inventor.views.welcome
   (:require [word-inventor.views.common :as common])
-  (:use [noir.core :only [defpage defpartial]]))
+  (:use [noir.core :only [defpage defpartial]]
+        [hiccup.element :only [link-to]]))
+
+(defpartial header []
+  [:div#header
+   [:p.header "Click on a language title to get a new random set of 'words' "
+    (link-to "https://www.github.com/bapehbe/word-inventor/tree/cljs-all" "(source code)")]])
 
 (defpage "/" []
   (common/layout
-   [:div {:id "languages"}]))
+   (header)
+   [:div#languages]))
